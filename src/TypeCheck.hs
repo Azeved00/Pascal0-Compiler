c7b8af48e1819a3cfaf4466be514958799744ec0
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2022-11-04 21:47:01 +0000

    smth

--- src/TypeCheck.hs
+++ src/TypeCheck.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE MultiWayIf #-}
 module TypeCheck where
 
 import Data.Map (Map)
@@ -12,4 +13,16 @@ checkStm :: Env -> Stm -> Bool
 checkStm _ _ = True
 
 checkExp :: Env -> Exp -> Type
-checkExp _ _ = TyBasic INTEGER 
+checkExp _ (Num _)  = TyBasic INTEGER
+checkExp _ (Bool _) = TyBasic BOOLEAN
+checkExp _ (Str _)  = TyBasic STRING
+--checkExp e (Id x)   = e ! x
+--checkExp e (BinOp o e1 e2) = case o of
+--    PLUS -> if
+--        | t1 == INTEGER && t2 == INTEGER -> tyBasic INTEGER
+--        | otherwise -> error
+
+-- do what t1 and t2 is
+-- do other operations
+-- maybe find better sintax 
+
