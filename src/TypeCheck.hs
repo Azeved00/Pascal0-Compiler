ddee2be0adf538b8a89d4da34e1413855627e4bf
up202004953 <up202004953@up.pt>
2022-11-06 10:12:57 +0000

    Procedure & for bug fix

--- src/TypeCheck.hs
+++ src/TypeCheck.hs
@@ -6,7 +6,9 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 
 type Env = Map String Type
-type EnvProc = Map String [Type]
+type EnvProc = Map String ([Type], Maybe Type)
+
+preludeProc = [("readint", [], Just (TyBasic INTEGER)), ("writeint", [TyBasic INTEGER], Nothing), ("readstr", [], Just (TyBasic STRING)), ("writestr", [TyBasic STRING], Nothing)]
 
 -- helper functions
 tcError :: String -> String -> a
@@ -14,10 +16,10 @@ tcError m s = error ("Type Error: " ++ m ++ " " ++ s ++ ".")
 
 findEnv :: Env -> String -> Type
 findEnv e s = case Map.lookup s e of
-                 Just t -> t 
+                 Just t -> t
                  Nothing -> tcError s "wasn't undefined"
 
-findProc :: EnvProc -> String -> [Type]
+findProc :: EnvProc -> String -> ([Type], Maybe Type)
 findProc e s = case Map.lookup s e of
                 Just t -> t
                 Nothing -> tcError s "wasn't defined"
@@ -25,36 +27,46 @@ findProc e s = case Map.lookup s e of
 checkType :: Type -> Type -> Bool
 checkType (TyBasic t1) (TyBasic t2) = t1 == t2
 checkType (TyArray t1 _ _) (TyArray t2 _ _) = t1 == t2
+checkType (TyBasic t1) (TyArray t2 _ _) = t1 == t2
+checkType (TyArray t1 _ _) (TyBasic t2) = t1 == t2
 
-paramType :: Param -> [Type]
-paramType EmptyParam = []
-paramType (Parameter _ t) = [t]
-paramType (CompoundParam p1 p2) = (paramType p1) ++ (paramType p2)
-
+paramType :: Env -> Param -> (Env,[Type])
+paramType env EmptyParam = (env, [])
+paramType env (Parameter s t) = (Map.insert s t env,[t])
+paramType env (CompoundParam p1 p2) = (e2,t1++t2)
+                                      where (e1,t1) = paramType env p1
+                                            (e2,t2) = paramType e1 p2
 
 --checkProgram
 checkProg :: Prog -> Bool
-checkProg (Program _ (Body c p v s)) = 
-    checkStm env' envp s
+checkProg (Program _ (Body c p v s)) =
+    checkStm env' envp' s
     where env  = constDef Map.empty c
-          envp = checkProc env Map.empty p
+          envp = foldr (\(s,t,r) m -> Map.insert s (t,r) m) Map.empty preludeProc
+          envp' = checkProc env envp p
           env' = varDef env v
 
 --check Procedure
 checkProc :: Env -> EnvProc -> Proc -> EnvProc
-checkProc env envp (Proc header body) 
-        | cond = procDef envp header
+checkProc env envp (Proc header body)
+        | cond = envp'
         | otherwise = tcError "Procedure was not defined correctly" (show header)
-        where cond = checkBody env envp body
-checkProc env envp (CompoundProc p1 p2) = 
+        where (env', envp') = procDef env envp header
+              cond = checkBody env' envp' body
+checkProc env envp (CompoundProc p1 p2) =
         checkProc env envp' p2
         where envp' = checkProc env envp p1
 checkProc env envp EmptyProc = envp
 
 -- define procedure
-procDef :: EnvProc -> ProcHeader -> EnvProc
-procDef envp (Procedure s p) = Map.insert s (paramType p) envp
-procDef envp (Function s p t) = Map.insert s (t : paramType p) envp
+procDef :: Env -> EnvProc -> ProcHeader -> (Env, EnvProc)
+procDef env envp (Procedure s p) = (env',envp')
+                                   where (env', typ) = paramType Map.empty p
+                                         envp' = Map.insert s (typ,Nothing) envp
+procDef env envp (Function s p t) = (env'', envp')
+                                    where env' = Map.insert s t Map.empty
+                                          (env'', typ) = paramType env' p
+                                          envp' = Map.insert s (typ,Just t) envp
 
 --check body
 checkBody :: Env -> EnvProc -> ProcBody -> Bool
@@ -77,49 +89,49 @@ constDef env (EmptyConst) = env
 -- check Parameters
 checkParam :: Env -> EnvProc -> Exp -> [Type] -> Bool
 checkParam _ _ EmptyExp [] = True
+checkParam _ _ EmptyExp _ = False
+checkParam _ _ _ [] = False
+checkParam env envp exp [typ] = checkType (checkExp env envp exp) typ
 checkParam env envp (CompoundExp e1 e2) (t1:t2) = (checkType (checkExp env envp e1) t1)
                                                   && (checkParam env envp e2 t2)
-checkParam _ _ (CompoundExp _ _) _ = False
-checkParam _ _ _ (_:_:_) = False
-checkParam env envp exp [typ] = checkType (checkExp env envp exp) typ
 
 -- Check Statment
 checkStm :: Env -> EnvProc -> Stm -> Bool
 checkStm env envp (BreakStm) = True
 checkStm env envp (CompoundStm stm1 stm2) = checkStm env envp stm1 && checkStm env envp stm2
-checkStm env envp (AssignStm (Id id) exp) = if
-        | checkType (checkExp env envp exp) typ -> True
-        | otherwise -> tcError ("error assigning " ++ (show exp) ++ " to") id 
-        where typ = findEnv env id
-checkStm env envp (IfStm cond stm) = if 
+checkStm env envp (AssignStm e1 e2) = if
+        | checkType (checkExp env envp e1) typ -> True
+        | otherwise -> tcError ("error assigning " ++ (show e2) ++ " to" ++ (show e1)) ""
+        where typ = (checkExp env envp e2)
+checkStm env envp (IfStm cond stm) = if
         | tycond && check -> True
-        | otherwise -> tcError "If Stm" 
+        | otherwise -> tcError "If Stm"
                 ("Condition: " ++ show tycond ++ " Statement: " ++ show check)
         where tycond = checkType (checkExp env envp cond) (TyBasic BOOLEAN)
               check = checkStm env envp stm
 checkStm env envp (IfElseStm cond stm1 stm2) = if
         | tycond && check1 && check2 -> True
-        | otherwise -> tcError "If Then Else Stm" 
-                ("Condition: " ++ show tycond ++ " Statement 1: " ++ show check1 ++ " statement 2: " ++ show check2) 
+        | otherwise -> tcError "If Then Else Stm"
+                ("Condition: " ++ show tycond ++ " Statement 1: " ++ show check1 ++ " statement 2: " ++ show check2)
         where tycond = checkType (checkExp env envp cond) (TyBasic BOOLEAN)
               check1 = checkStm env envp stm1
               check2 = checkStm env envp stm2
-checkStm env envp (WhileStm cond stm) = if 
+checkStm env envp (WhileStm cond stm) = if
         | tycond && check -> True
         | otherwise -> tcError "While" ("Condition: " ++ show tycond ++ " Statement: " ++ show check)
         where tycond = checkType (checkExp env envp cond) (TyBasic BOOLEAN)
               check = checkStm env envp stm
 checkStm env envp (ForStm stm1 cond stm2) = if
         | tycond && check1 && check2 -> True
-        | otherwise -> tcError "For" 
-                ("Statement 1: " ++ show check1 ++ " Condition: " ++ show tycond ++ " statement 2: " ++ show check2)        
-        where tycond = checkType (checkExp env envp cond) (TyBasic BOOLEAN)
+        | otherwise -> tcError "For"
+                ("Statement 1: " ++ show check1 ++ " Condition: " ++ show tycond ++ " statement 2: " ++ show check2)
+        where tycond = checkType (checkExp env envp cond) (TyBasic INTEGER)
               check1 = checkStm env envp stm1
               check2 = checkStm env envp stm2
-checkStm env envp (ProcStm name exp) = if 
+checkStm env envp (ProcStm name exp) = if
         | checkParam env envp exp typ -> True
         | otherwise -> tcError "Parameters were incorrect for procedure" name
-        where typ = findProc envp name
+        where (typ,_) = findProc envp name
 
 -- Check Expression
 checkExp :: Env -> EnvProc -> Exp -> Type
@@ -130,8 +142,8 @@ checkExp e _ (Id x)      = findEnv e x
 checkExp ev ep (Array x e) = if
     | checkType (checkExp ev ep e) (TyBasic INTEGER) -> findEnv ev x
     | otherwise -> tcError "Incorrect Array Access" x
-checkExp e p (UnOp o e1) = 
-    let t = checkExp e p e1 
+checkExp e p (UnOp o e1) =
+    let t = checkExp e p e1
     in if
         | o == MINUS -> if
             | checkType t int -> int
@@ -142,9 +154,9 @@ checkExp e p (UnOp o e1) =
         | otherwise -> tcError "Operation is not Unary" (show o)
         where int = TyBasic INTEGER
               bool = TyBasic BOOLEAN
-checkExp e p (BinOp o e1 e2) = 
-    let t1 = checkExp e p e1 
-        t2 = checkExp e p e1 
+checkExp e p (BinOp o e1 e2) =
+    let t1 = checkExp e p e1
+        t2 = checkExp e p e1
     in if
         | elem o [PLUS, MINUS, MULT, DIV, MOD] -> if
             | checkType t1 int && checkType t2 int -> TyBasic INTEGER
@@ -158,11 +170,10 @@ checkExp e p (BinOp o e1 e2) =
         | otherwise -> tcError "Operation is not Binary" (show o)
         where int = TyBasic INTEGER
               bool = TyBasic BOOLEAN
-checkExp ev ep (Func i p) = 
-    let types = findProc ep i
-        retType = head types
-        parType = tail types
-    in if
-        | checkParam ev ep p parType -> retType 
-        | otherwise -> tcError "parameters are not the same for" i 
-        
+
+checkExp ev ep (Func i p) =
+    let (typ,ret) = findProc ep i
+    in case ret of
+      Just r -> if | checkParam ev ep p typ -> r
+                   | otherwise -> tcError "parameters are not the same for" i
+      Nothing -> tcError "wrong use of procedure" i
