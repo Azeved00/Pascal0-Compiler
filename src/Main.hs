728206356b2ef6ae7a8e93ba61392b2ce10d6c2c
up202004953 <up202004953@up.pt>
2022-12-04 23:45:45 +0000

    Updating Main

--- src/Main.hs
+++ src/Main.hs
@@ -3,10 +3,13 @@ import DataTypes
 import Lexer
 import Parser
 import TypeCheck
+import Generator
 --alexScanTokens :: String -> [Tokens]
 
 
 main = do
     txt <- getContents
-    putStrLn $ show $ parse $ alexScanTokens txt
-    putStrLn $ show $ checkProg $ parse $ alexScanTokens txt
+    putStrLn $ show $ alexScanTokens txt
+    --putStrLn $ show $ parse $ alexScanTokens txt
+    --putStrLn $ show $ checkProg $ parse $ alexScanTokens txt
+    --putStrLn $ show $ generate $ parse $ alexScanTokens txt
