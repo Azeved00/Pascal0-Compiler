08b8f6c4a8671fcf08f86a8735f6d2b7de57270d
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2022-12-09 15:28:26 +0000

    need to switch pc, nothing is done

--- src/MachineGenerator.hs
+++ src/MachineGenerator.hs
@@ -2,4 +2,39 @@ module MachineGenerator where
 
 import DataTypes
 
-genMachineCode :: Data -> [Instr] -> String
+data Instr = MOVE Temp Temp
+           | MOVEI Temp Int
+           | MOVES Temp Label
+           | OPER Op Temp Temp Temp
+           | OPERI Op Temp Temp Int
+           | LABEL Label
+           | JUMP Label
+           | COND Temp Op Temp Label Label
+           | CONDI Temp Op Int Label Label
+           | CALL Temp Label [Temp]
+           | LOAD Temp Int Temp
+           | SAVE Temp Int Temp
+           | RETURN Temp
+
+type ICode = ([Def], [Instr])
+
+-----------------------------Funcoes de Auxiliares-----------------------------------
+writeLine :: Bool -> String -> String
+writeLine tab msg = if (tab) then ("\t" ++ msg ++ "\n") else (msg ++ "\n")
+
+
+-----------------------------Geracao de codigo maquina--------------------------------
+genMachineCode :: ICode -> String
+genMachineCode (defs,instrs) = (writeLine False ".data")++(genData defs) ++ (writeLine False ".text")++(genInstr instrs)
+
+
+genData :: [Def] -> String
+genData [] = ""
+genData ((DARRAY label size):xs) = writeLine true ((show label)++": .space "++(show size)) 
+genData d = error ("Error: Can't parse Definition" ++ show d)
+
+genInstr :: [Instr] -> String
+genInstr [] = ""
+genInstr i = error ("Error: Can't parse Instruction -> " ++ show i)
+
+
