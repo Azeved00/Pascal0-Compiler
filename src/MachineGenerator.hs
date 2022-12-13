e16e02e1c4fff347dcfd6b54d6e7e125fbfd2ce7
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2022-12-13 17:25:26 +0000

    finished special procs

--- src/MachineGenerator.hs
+++ src/MachineGenerator.hs
@@ -9,26 +9,54 @@ writeLine tab msg = if (tab) then ("    " ++ msg ++ "\n") else (msg ++ "\n")
 writeInstr:: String -> [String] -> String
 writeInstr cmd params = writeLine True (cmd ++ (foldl (\a b -> a ++ " " ++ b) " " params))
 
+takeThings :: String -> String
+takeThings a = init (tail a)
 -----------------------------Geracao de codigo maquina--------------------------------
 genMachineCode :: ICode -> String
 genMachineCode (defs,instrs) = 
     (writeLine False ".data")++(genData defs) ++ 
     (writeLine False ".text")++(genInstr instrs)
 
-
+----------------GENERATE DATA------------------------
 genData :: [Def] -> String
 genData [] = ""
-genData ((DARRAY label size):xs) = (writeLine True (show label)) ++ ": .space " ++ (show size) 
-genData d = error ("Error: Can't parse Definition" ++ show d)
+genData ((DARRAY label size):xs) = 
+    "    " ++ (label) ++ ": .space " ++ (show size) ++ "\n" ++ (genData xs)
+genData ((DSTRING label str):xs) = 
+    "    " ++ (label) ++ ": .asciiz " ++ ( "\""++ (takeThings str) ++ "\"")++ " \n" ++ (genData xs) 
 
+
+-----------------GENERATE INSTRUCTIONS-------------
 genInstr :: [Instr] -> String
 genInstr [] = ""
 
+--------------------SPECIFIC CASES -----------------
+genInstr ((CALLP "writeint" [i]):xs) = 
+    (writeInstr "li" ["$v0","1"]) ++
+    (writeInstr "move" ["$a0",i]) ++
+    (writeInstr "syscall" []) ++
+    (genInstr xs)
+
+genInstr ((CALLP "writestr" [i]):xs) = 
+    (writeInstr "li" ["$v0","4"]) ++
+    (writeInstr "move" ["$a0",i]) ++
+    (writeInstr "syscall" []) ++
+    (genInstr xs)
+
+
+genInstr ((CALLF dest "readint" _):xs) = 
+    (writeInstr "li" ["$v0","5"]) ++
+    (writeInstr "syscall" []) ++
+    (writeInstr "move" [dest,"$v0"])++
+    (genInstr xs)
+----------------------BASIC JUMPS-------------------
 genInstr ((LABEL l):xs)     = (writeLine False (l++":")) ++ (genInstr xs)
 genInstr ((JUMP l):xs)      = (writeInstr "j" [l]) ++ (genInstr xs)
 
+--------------------MOVE AND OPERATIONS-------------
 genInstr ((MOVE t1 t2):xs)  = (writeInstr "move"  [t1,t2]) ++ (genInstr xs)
 genInstr ((MOVEI t1 i):xs)  = (writeInstr "li" [t1,show i]) ++ (genInstr xs)
+genInstr ((MOVES t1 i):xs)  = (writeInstr "la" [t1,i]) ++ (genInstr xs)
 
 genInstr ((OPER PLUS dest t1 t2):xs) = (writeInstr "add" [dest,t1,t2]) ++ (genInstr xs)
 genInstr ((OPERI PLUS dest t1 i):xs) = (writeInstr "addi" [dest,t1,show i]) ++ (genInstr xs)
@@ -39,6 +67,7 @@ genInstr ((OPER MOD dest t1 t2):xs) =
     (writeInstr "div" [t1,t2]) ++ 
     (writeInstr "mdhi" [dest]) ++ (genInstr xs)
 
+-----------------CONDITIONS------------------------
 genInstr ((COND t1 EQUAL t2 lt lf):(LABEL l1):xs) 
     | l1 == lt = (writeInstr "bne" [t1,t2,lf]) ++ cont
     | l1 == lf = (writeInstr "beq" [t1,t2,lt]) ++ cont
@@ -73,14 +102,3 @@ genInstr ((COND t1 LEQUAL t2 lt lf):(LABEL l1):xs)
 genInstr (x:xs) = (writeLine True (show x)) ++ (genInstr xs)  
 --error ("Error: Can't parse Instruction -> " ++ show i)
 
-{-
-data Instr =
-           | MOVES Temp Label
-           | OPERI Op Temp Temp Int
-           | COND Temp Op Temp Label Label
-           | CONDI Temp Op Int Label Label
-           | CALL Temp Label [Temp]
-           | LOAD Temp Int Temp
-           | SAVE Temp Int Temp
-           | RETURN Temp
--}
